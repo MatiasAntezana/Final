b4db76117f8842d89c953633f25076057377ae66
PedroJensen <pjensen@udesa.edu.ar>
2022-07-06 06:31:29 -0300

    Entrar

--- setup.py
+++ setup.py
@@ -5,20 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name = "MOTHER_MAIN" ,
     version = "1.0.0" ,
-    packages=find_packages(
-        where=".",
-        include=[],
-    )
+    author= "Matias Antezana, Pedro Jensen",
+    packages=find_packages()
 )
 
-setup(
-    # ..., 
-    setup_requires=["flake8"] 
-)
-
- 
-setup(
-    # ...,
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-)
